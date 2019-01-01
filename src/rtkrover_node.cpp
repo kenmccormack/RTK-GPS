5ddbcf39d06f201b96c072ef78db52b801049fb2
ken <her@home.com>
2019-01-01 17:19:31 +0000

    publishes to ROS.  Local decode of messages

--- src/rtkrover_node.cpp
+++ src/rtkrover_node.cpp
@@ -27,8 +27,98 @@ std::mutex radio_mutex;
 #define NUM_BYTES 200
 
 async_comm::Serial gps_serial("/dev/ttyUSB0", 57600);
+ros::NodeHandle * nhp; 
+ros::Publisher pub_odm;
 
 std::string msg_buffer; 
+
+
+
+class DecodeBinaryStream
+{
+  public:
+
+  
+
+    char last_byte;
+    int state;
+    char body_length[2];
+    char body_buf[500];
+    int body_index; 
+
+    DecodeBinaryStream()
+    {
+      last_byte=0;
+      state = HEADER;
+      body_index=0;
+    }
+
+    void decode(const unsigned char *buf, int len)
+    {
+    //parse the protocol*/ 
+      for(int ii=0;ii<len;ii++)
+      {
+        switch(state)
+        {
+          case HEADER: 
+            if (last_byte == 0xA0 && buf[ii]==0xA1)
+                state = LENGTH1;
+            break;
+
+          case LENGTH1:
+            body_length[0] = buf[ii];
+            state = LENGTH2;
+            break;
+
+          case LENGTH2:
+            body_length[1] = buf[ii];
+            state = BODY;
+            body_index = 0; 
+            break;
+
+          case BODY:
+            body_buf[body_index] = buf[ii];
+            body_index++;
+            if(body_index >= ( body_length[0]*256 + body_length[1]) )
+              state = CRC;
+            break;
+
+          case CRC:
+            if(body_buf[0] == 0xdf)
+            {
+              printf("Message id 0x%x, len=%d, (0x%x,0x%x,0x%x,0x%x)\n",
+                    body_buf[0],
+                    (body_length[0]*256 + body_length[1]) ,
+                    body_buf[1],
+                    body_buf[2],
+                    body_buf[3],
+                    body_buf[4] );
+            }
+            state = HEADER;
+
+            break;
+
+          default :
+            state = HEADER;
+            break;
+        }
+
+      last_byte = buf[ii];
+      
+      } 
+    }
+
+    private:
+      enum
+      {
+        HEADER,
+        LENGTH1,
+        LENGTH2,
+        BODY,
+        CRC
+      };
+
+};
 
 
 void process_gps_msg(std::string msg)
@@ -42,19 +132,36 @@ void process_gps_msg(std::string msg)
     msg_elements.push_back(t); 
   }
 
-  ROS_INFO("Token count = %d", (int)msg_elements.size());
-
   if(msg_elements.size() > 1)
   {
-    ROS_INFO("%s",msg_elements[0].c_str());
+    
     if (msg_elements[0] == "$PSTI" && msg_elements[1]=="032")
     {
+
+      ROS_INFO("GPS message %s", msg.c_str());
+
+      /*for (int ii=0 ; ii<msg_elements.size(); ii++)
+	ROS_INFO("%d %s",ii,msg_elements[ii].c_str());
+      */
+
       if (msg_elements[4]=="A")
       {
           ROS_INFO("Valid Position --> East: %s North: %s Up: %s", 
             msg_elements[6].c_str(),
             msg_elements[7].c_str(),
             msg_elements[8].c_str());
+          
+          /*go ahead and publish this message to ROS*/ 
+          nav_msgs::Odometry odm_msg;
+          odm_msg.header.stamp = ros::Time::now(); 
+          odm_msg.header.frame_id = "rover";
+          odm_msg.pose.pose.position.x = stof(msg_elements[6]);
+          odm_msg.pose.pose.position.y = stof(msg_elements[7]);
+          odm_msg.pose.pose.position.z = stof(msg_elements[8]);
+
+           
+          pub_odm.publish(odm_msg);
+
       }
       else
       {
@@ -76,6 +183,7 @@ void process_gps_msg(std::string msg)
  */
 void gps_callback(const uint8_t* buf, size_t len)
 {
+ 
   for (size_t i = 0; i < len; i++)
   {
     //std::printf("%c", buf[i]);
@@ -91,21 +199,36 @@ void gps_callback(const uint8_t* buf, size_t len)
 
 void radio_callback(const uint8_t* buf, size_t len)
 {
+     static DecodeBinaryStream dbs;
+     static int rx_count;
 
     /*send to the gps input */ 
 #ifdef GPS_SERIAL
     gps_serial.send_bytes(buf, len);
 #endif
-  
-    //std::printf("radio received %d bytes",(int)len);
-  
+
+    //#dbs.decode(buf,len);
+
+    if (rx_count%255 == 0)
+    {
+	    ROS_INFO("Rx'd rtk bytes.");
+    } 
+    rx_count++;
+
 }
+
+
 
 
 int main(int argc, char** argv)
 {
   ros::init(argc , argv, "rtkrover");
 
+  ros::NodeHandle nh; 
+  nhp = &nh;
+
+  pub_odm = nh.advertise<nav_msgs::Odometry>("/rover_odm", 10 );
+      
 
   // open serial port
   
@@ -118,7 +241,7 @@ int main(int argc, char** argv)
   }
   else
   {
-    std::printf("GPS Serial has been initialized");
+    std::printf("GPS Serial has been initialized\n");
   }
 #endif 
 
